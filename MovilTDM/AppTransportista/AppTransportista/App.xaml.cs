2261912c1b738a2a12c6d021f6704c4a77c87e9d
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-11-25 18:42:57 -0500

    Configuracion MasterPage y color INICIAL

--- MovilTDM/AppTransportista/AppTransportista/App.xaml.cs
+++ MovilTDM/AppTransportista/AppTransportista/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppTransportista.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,13 @@ namespace AppTransportista
 {
     public partial class App : Application
     {
+        public static NavigationPage Navigator { get; internal set; }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppTransportista.MainPage();
+            MainPage = new MasterPage();
         }
 
         protected override void OnStart()
