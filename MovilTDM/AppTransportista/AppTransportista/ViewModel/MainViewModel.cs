8943e822ab9b9d88b21efe0f04219929ee5236fb
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-11-26 09:35:17 -0500

    87 ya configurate gesture reconizer para navegacion

--- MovilTDM/AppTransportista/AppTransportista/ViewModel/MainViewModel.cs
+++ MovilTDM/AppTransportista/AppTransportista/ViewModel/MainViewModel.cs
@@ -1,17 +1,23 @@
-﻿using System;
+﻿using AppTransportista.Pages;
+using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AppTransportista.ViewModel
 {
     public class MainViewModel//la reina detodas las clases
     {//oservable necesito verlo desde la vista del menu
+        #region Properties
         public ObservableCollection<MenuItemViewModel> Menu { get; set; }
 
         public ObservableCollection<OrderViewModel> Orders { get; set; }
+        #endregion
+        #region Constructors
         public MainViewModel()
         {
             Menu = new ObservableCollection<MenuItemViewModel>();
@@ -19,15 +25,18 @@ namespace AppTransportista.ViewModel
             LoadMenu();
             LoadFakeData();
         }
+        #endregion
+        #region Methods
 
         private void LoadFakeData()
         {
             for (int i = 0; i < 10; i++)
             {
-                Orders.Add(new OrderViewModel {
-                    Title="Lorem Ipsum",
-                    DeliveryDate=DateTime.Today,
-                    Description="Lorem Ipsum dolor sit amet" +
+                Orders.Add(new OrderViewModel
+                {
+                    Title = "Lorem Ipsum",
+                    DeliveryDate = DateTime.Today,
+                    Description = "Lorem Ipsum dolor sit amet" +
                     "amet dolor sip elt,se ionjnkjnkjnjknkjnkj" +
                     "jnjnjnkjnjknkj"
                 });
@@ -37,37 +46,53 @@ namespace AppTransportista.ViewModel
         private void LoadMenu()
         {//add los elementos del menu
             Menu.Add(
-                new MenuItemViewModel {
+                new MenuItemViewModel
+                {
                     Icon = "ic_action_orders.png",
-                    Title="MainPage",
-                    PageName="Pedidos"
+                    PageName = "MainPage",
+                     Title = "Pedidos"
                 }
                 );
             Menu.Add(
                new MenuItemViewModel
                {
                    Icon = "ic_action_customers.png",
-                   Title = "ClientsPage",
-                   PageName = "Clientes"
+                   PageName = "ClientsPage",
+                   Title = "Clientes"
                }
                );
             Menu.Add(
                new MenuItemViewModel
                {
                    Icon = "ic_action_alarms.png",
-                   Title = "AlarmsPage",
-                   PageName = "Alarmas"
+                   PageName = "AlarmsPage",
+                    Title = "Alarmas"
                }
                );
             Menu.Add(
                new MenuItemViewModel
                {
                    Icon = "ic_action_settings.png",
-                   Title = "SettingsPage",
-                   PageName = "Ajustes"
+                   PageName = "SettingsPage",
+                    Title = "Ajustes"
                }
                );
 
         }
+        #endregion
+
+        #region Commands
+        public ICommand GotoCommand { get { return new RelayCommand<string>(Goto); } }
+
+        private void Goto(string pageName)
+        {
+            switch (pageName)
+            {
+                case "NewOrderPage":
+                    App.Navigator.PushAsync(new NewOrderPage());break;
+                default:;break;
+            }
+        }
+        #endregion
     }
 }
--- MovilTDM/AppTransportista/AppTransportista/ViewModel/MenuItemViewModel.cs
+++ MovilTDM/AppTransportista/AppTransportista/ViewModel/MenuItemViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using AppTransportista.Pages;
+using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AppTransportista.ViewModel
 {
@@ -11,5 +14,29 @@ namespace AppTransportista.ViewModel
         public string Icon { get; set; }
         public string Title { get; set; }
         public string PageName { get; set; }
+
+        /******la lista******Dentro del listview de menu*/
+        public ICommand NavigateCommand { get { return new RelayCommand(Navigate); } }
+
+        private void Navigate()
+        {
+            switch (PageName)
+            {
+                case "AlarmsPage":
+                    App.Navigator.PushAsync(new AlarmsPage());
+                    break;
+                case "ClientsPage":
+                    App.Navigator.PushAsync(new ClientsPage());
+                    break;
+                case "SettingsPage":
+                    App.Navigator.PushAsync(new SettingsPage());
+                    break;
+                case "MainPage":
+                    App.Navigator.PopToRootAsync();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
