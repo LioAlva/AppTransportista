1a748eaa88c79f518b672223acaadcadc3d21d4f
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-11-26 02:18:39 -0500

    85 Recien Instalado libreria stance locator

---
+++ MovilTDM/AppTransportista/AppTransportista/ViewModel/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTransportista.ViewModel
+{
+    public class MainViewModel//la reina detodas las clases
+    {//oservable necesito verlo desde la vista del menu
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+
+        public ObservableCollection<OrderViewModel> Orders { get; set; }
+        public MainViewModel()
+        {
+            Menu = new ObservableCollection<MenuItemViewModel>();
+            Orders = new ObservableCollection<OrderViewModel>();
+            LoadMenu();
+            LoadFakeData();
+        }
+
+        private void LoadFakeData()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Orders.Add(new OrderViewModel {
+                    Title="Lorem Ipsum",
+                    DeliveryDate=DateTime.Today,
+                    Description="Lorem Ipsum dolor sit amet" +
+                    "amet dolor sip elt,se ionjnkjnkjnjknkjnkj" +
+                    "jnjnjnkjnjknkj"
+                });
+            }
+        }
+
+        private void LoadMenu()
+        {//add los elementos del menu
+            Menu.Add(
+                new MenuItemViewModel {
+                    Icon = "ic_action_orders.png",
+                    Title="MainPage",
+                    PageName="Pedidos"
+                }
+                );
+            Menu.Add(
+               new MenuItemViewModel
+               {
+                   Icon = "ic_action_customers.png",
+                   Title = "ClientsPage",
+                   PageName = "Clientes"
+               }
+               );
+            Menu.Add(
+               new MenuItemViewModel
+               {
+                   Icon = "ic_action_alarms.png",
+                   Title = "AlarmsPage",
+                   PageName = "Alarmas"
+               }
+               );
+            Menu.Add(
+               new MenuItemViewModel
+               {
+                   Icon = "ic_action_settings.png",
+                   Title = "SettingsPage",
+                   PageName = "Ajustes"
+               }
+               );
+
+        }
+    }
+}
---
+++ MovilTDM/AppTransportista/AppTransportista/ViewModel/MenuItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTransportista.ViewModel
+{
+    public class MenuItemViewModel//un item de la lista menu
+    {
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string PageName { get; set; }
+    }
+}
